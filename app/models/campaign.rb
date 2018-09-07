4ba068a7062228e0a25f5aab7445eb6f4f552ad0
Rachel Miller <raeanne1991@gmail.com>
2018-09-07 15:57:26 -0400

    added in validations

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,5 @@
 class Campaign < ApplicationRecord
   has_many :comments, dependent: :destroy
+
+  validates :title, :description, :goal, :pleged_amount, presence: true
 end
--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -5,5 +5,5 @@ RSpec.describe Campaign, type: :model do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
   it { should validate_presence_of(:goal) }
-  it { should validate_presence_of(:pledged) }
+  it { should validate_presence_of(:pleged_amount) }
 end
